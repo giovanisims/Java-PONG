3a6ef0611fd99a633ff50d742426a46a8cf3a78d
Lucas-BZan <lucasbrisch@gmail.com>
2024-10-30 21:30:17 -0300

    Definitive version

--- Main.java
+++ Main.java
@@ -10,23 +10,3 @@ public class Main {
     }
 }
 
-<<<<<<< HEAD
-=======
-    private static void timerRefresh(PongGame game) {
-        int DELAY = 33;
-
-        Timer timerRefresh = new Timer(DELAY, new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-
-                game.gameLogic();
-
-                game.repaint();
-
-            }
-        });
-
-        timerRefresh.start();
-    }
-}
->>>>>>> 0f7507fd06bc90d36da7414f760b9b57d350e2ff
